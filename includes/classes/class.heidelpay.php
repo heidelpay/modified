bc27c2b045cb86516a5823decb5993b86b3e1d43
Jens Richter <jr@frostblog.net>
2017-04-06 15:33:50 +0200

    add ivsec to direct node

--- includes/classes/class.heidelpay.php
+++ includes/classes/class.heidelpay.php
@@ -196,7 +196,8 @@ class heidelpay
             echo '<pre>SESSION: ' . print_r($_SESSION['hpLastData'], 1) . '</pre>';
         }
         $capture = false;
-        if (strtoupper($payCode) == 'DD' or strtoupper($payCode) == 'DDSEC') {
+        $upperPayCode = strtoupper($payCode);
+        if ( $upperPayCode == 'DD' or $upperPayCode == 'DDSEC' or $upperPayCode == 'IVSEC') {
             $ACT_MOD_MODE = 'DIRECT';
         }
         if ($ACT_MOD_MODE == 'DIRECT') {
